98a08b7e3407b5586e74a2381d4b0b6259fe0e34
Pawel Yarko <pawelyarko@gmail.com>
2022-06-10 23:04:54 +0200

    form options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Form from './Form/Form'
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import s from './PhoneBook.module.css';
+
+export default class App extends React.Component{
+  state = {
+    contacts: [],
+    filter: '',
+  }  
+
+  elemId = nanoid();
+  
+  formSubmitHandler = (data) =>{
+    this.state.contacts.push(data);
+    // console.log(this.state.contacts)
+  }
+
+  
+  
+  render(){
+    console.log(this.state.contacts)
+    return (
+      <div className={s.container}>
+        <h1>Phonebook</h1>
+      <Form formData={this.formSubmitHandler}/> 
+        <div>
+          <h2>Contacts</h2>
+          <ul>{this.state.contacts.map(contact =>{
+            return <li key={contact.id}>{contact.name}: {contact.number}</li>
+          })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
